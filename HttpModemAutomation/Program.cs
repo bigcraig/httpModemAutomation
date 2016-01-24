2de408c78a6ab1d453ad3490c6cbdf5946e9ad76
Craig Woollett <craigwoollett@outlook.com>
2016-01-25 02:25:51 +1100

    added basic auth to  login for nf4v

--- HttpModemAutomation/Program.cs
+++ HttpModemAutomation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBrowser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,22 @@ namespace HttpModemAutomation
     class Program
     {
         static void Main(string[] args)
+
         {
+           var modem = new Browser();
+            var modemURL = "http://192.168.20.1:8080";
+                modem.Navigate(modemURL);
+            //    Console.Write("get url");
+            //    modem.BasicAuthenticationLogin("Broadband Router","admin","admin");
+            modem.BasicAuthenticationLogin("192.168.20.1", "admin", "admin");
+
+
+            modem.Navigate(modemURL);
+                Console.Write(modem.CurrentHtml);
+           // Console.Write(modem.)
+            Console.Read();
+
+
         }
     }
 }
