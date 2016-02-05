66c0aecf3cdcb4fbad5cf9442288304bac87524e
Craig Woollett <craigwoollett@outlook.com>
2016-02-06 03:06:30 +1100

    get cgi for acs configuration

--- HttpModemAutomation/Program.cs
+++ HttpModemAutomation/Program.cs
@@ -18,11 +18,26 @@ namespace HttpModemAutomation
 
         }
 
+          static string  ACSConfigString(string sessionKey)
+             /* string Tr69cAcsPwd,
+              string tr69cConnReqUser,
+              string tr69cConnReqPwd,
+              string tr69cConnReqPort,
+              string tr69cAcsUrl,
+              string informInterval) */
+        {
+            string _acspost;
+            _acspost = "tr69cfg.cgi?tr69cEnable=1&tr69cInformEnable=1&tr69cInformInterval=86423&tr69cAcsURL=http://acs.netcommwireless.com/cpe.php&tr69cAcsUser=cpe&tr69cAcsPwd=cpe&tr69cConnReqUser=cpe&tr69cConnReqPwd=cpe&tr69cConnReqPort=30005&tr69cNoneConnReqAuth=0&tr69cDebugEnable=0&tr69cBoundIfName=Any_WAN&sessionKey=";
+            _acspost = _acspost + sessionKey;
+           // Console.Write(_acspost);
+            return (_acspost);
+        }
+
         static void Main(string[] args)
 
         {
            var modem = new Browser();
-            var modemURL = "http://192.168.20.1:8080/tr69cfg.html";
+            var modemURL = "http://192.168.20.1/tr69cfg.html";
                 modem.Navigate(modemURL);
             //    Console.Write("get url");
             //    modem.BasicAuthenticationLogin("Broadband Router","admin","admin");
@@ -30,18 +45,23 @@ namespace HttpModemAutomation
 
 
             modem.Navigate(modemURL);
-                Console.Write(modem.CurrentHtml);
+           //     Console.Write(modem.CurrentHtml);
             var html = modem.CurrentHtml;
 
             string pattern = @"var sessionKey='([^']+)';";
             Match match = Regex.Match(html, pattern);
             string sessionKey = match.Groups[1].Value;
-            Console.Write(sessionKey);
+           // Console.Write(sessionKey);
             sessionKey = getSessionID(html);
-            Console.Write(sessionKey);
-            Console.Read();
+           // Console.Write(sessionKey);
+           // Console.Read();
+            // create response
 
+            // tr69cfg.cgi?tr69cEnable=1&tr69cInformEnable=1&tr69cInformInterval=86402&tr69cAcsURL=http://acs.netcommwireless.com/cpe.php&tr69cAcsUser=cpe&tr69cAcsPwd=cpe&tr69cConnReqUser=cpe&tr69cConnReqPwd=cpe&tr69cConnReqPort=30005&tr69cNoneConnReqAuth=0&tr69cDebugEnable=0&tr69cBoundIfName=Any_WAN&sessionKey=1045881970
 
+            var getstringACS = ACSConfigString(sessionKey);
+            modemURL = modemURL + getstringACS;
+            modem.Navigate(modemURL);
         }
     }
 }
